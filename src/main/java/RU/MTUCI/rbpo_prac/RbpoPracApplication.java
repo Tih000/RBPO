afb6f76d24f9bd16da0f4bff7fc7d8e2976001fe
Tih000 <tbludov2004@gmail.com>
2024-11-09 15:25:48 +0300

    Update RbpoPracApplication.java

--- src/main/java/RU/MTUCI/rbpo_prac/RbpoPracApplication.java
+++ src/main/java/RU/MTUCI/rbpo_prac/RbpoPracApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RbpoPracApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RbpoPracApplication.class, args);
 	}
-
 }
