e04ed49326f4e280964b60d489903fd24608f3f0
Tih000 <tbludov2004@gmail.com>
2024-11-09 15:26:02 +0300

    Update mainClass.java

--- src/main/java/RU/MTUCI/rbpo_prac/mainClass.java
+++ src/main/java/RU/MTUCI/rbpo_prac/mainClass.java
@@ -10,19 +10,15 @@ public class mainClass {
         this.name = name;
         this.number = number;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getNumber() {
         return number;
     }
-
     public void setNumber(int number) {
         this.number = number;
     }
